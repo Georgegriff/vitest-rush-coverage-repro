de6b4401fb636a4717841f9326b799791fd722cc
Georgegriff <george.griffiths@immersivelabs.com>
2025-10-02 12:47:31 +0100

    dont run coverage on config

--- apps/vitest-react-app/vitest.config.ts
+++ apps/vitest-react-app/vitest.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "src/test/"],
+      include: ["**/src/**/*.{ts,tsx}"],
     },
   },
   optimizeDeps: {
